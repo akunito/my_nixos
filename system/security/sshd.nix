e0f99bd992296422d735fc81be9750a886315ffd
akunito <diego88aku@gmail.com>
2024-07-31 12:35:03 +0200

    debugging sshd issues

--- system/security/sshd.nix
+++ system/security/sshd.nix
@@ -12,7 +12,7 @@
     extraConfig = ''
       Port 34389                          # Use a non-default port
       ListenAddress 192.168.0.80:34389    # Bind to the new port
-      ListenAddress [::]:34389
+      #ListenAddress [::]:34389
       AllowUsers akunito                  # Allow only specific user
       MaxAuthTries 3                      # Limit authentication attempts
       LoginGraceTime 30s                  # Reduce grace time
