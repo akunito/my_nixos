e9a5c3c91903f7e8eb07a0c8705967c43ef436d5
akunito <>
2024-08-06 08:21:57 +0200

    all ssh keys updated to rsa
    fixing dhcp conflicts

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -41,7 +41,7 @@
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eth0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp9s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
--- system/hardware/drives.nix
+++ system/hardware/drives.nix
@@ -1,6 +1,22 @@
-{ ... }:
+{ config, pkgs, ... }:
 
-{
+{ 
+
+  # # Reconnect network
+  # systemd.services.reconnect-network = {
+  #   description = "Reconnect network interface";
+  #   after = [ "network.target" "NetworkManager.service" ];
+  #   wantedBy = [ "multi-user.target" ];
+
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     ExecStart = ''
+  #       ${pkgs.iproute}/bin/ip link set eno1 down && sleep 5 && ${pkgs.iproute}/bin/ip link set eno1 up
+  #     '';
+  #     RemainAfterExit = true;
+  #   };
+  # };
+
   # # Open DATA_4TB LUKS on Boot (it needs pass or keyfile on an uncrypted drive)
   # boot.initrd.luks.devices.DATA_4TB = {
   #   device = "/dev/disk/by-uuid/231c229c-1daf-43b5-85d0-f1691fa3ab93";
@@ -28,8 +44,17 @@
   #     fsType = "ext4";
   #   };
 
+  # Static IP # don't reserve the DHCP address on Router config, or you might have conflicts
+  networking.interfaces.eth0.ipv4.addresses = [ { # check that eth0 is the right interface to use
+    address = "192.168.0.80";
+    prefixLength = 24;
+  } ];
+  networking.hostName = "nixosaku";
+  networking.defaultGateway = "192.168.0.1";
+  networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
+
   # SSH on Boot > https://nixos.wiki/wiki/Remote_disk_unlocking
-  boot.kernelParams = [ "ip=192.168.0.80::192.168.0.1:255.255.255.0:myhost::none" ]; # where 192.168.0.80 is the IP of myhost
+  boot.kernelParams = [ "ip=dhcp" ];
   boot.initrd = {
     availableKernelModules = [ "r8169" ];
     systemd.users.root.shell = "/bin/cryptsetup-askpass";
@@ -39,7 +64,8 @@
         enable = true;
         port = 22;
         authorizedKeys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCfNRaYr4LSuhcXgI97o2cRfW0laPLXg7OzwiSIuV9N7cin0WC1rN1hYi6aSGAhK+Yu/bXQazTegVhQC+COpHE6oVI4fmEsWKfhC53DLNeniut1Zp02xLJppHT0TgI/I2mmBGVkEaExbOadzEayZVL5ryIaVw7Op92aTmCtZ6YJhRV0hU5MhNcW5kbUoayOxqWItDX6ARYQov6qHbfKtxlXAr623GpnqHeH8p9LDX7PJKycDzzlS5e44+S79JMciFPXqCtVgf2Qq9cG72cpuPqAjOSWH/fCgnmrrg6nSPk8rLWOkv4lSRIlZstxc9/Zv/R6JP/jGqER9A3B7/vDmE8e3nFANxc9WTX5TrBTxB4Od75kFsqqiyx9/zhFUGVrP1hJ7MeXwZJBXJIZxtS5phkuQ2qUId9zsCXDA7r0mpUNmSOfhsrTqvnr5O3LLms748rYkXOw8+M/bPBbmw76T40b3+ji2aVZ4p4PY4Zy55YJaROzOyH4GwUom+VzHsAIAJF/Tg1DpgKRklzNsYg9aWANTudE/J545ymv7l2tIRlJYYwYP7On/PC+q1r/Tfja7zAykb3tdUND1CVvSr6CkbFwZdQDyqSGLkybWYw6efVNgmF4yX9nGfOpfVk0hGbkd39lUQCIe3MzVw7U65guXw/ZwXpcS0k1KQ+0NvIo5Z1ahQ== akunito@Diegos-MacBook-Pro.local" ]; # update with your client key
-        hostKeys = [ "/etc/secrets/initrd/ssh_host_rsa_key" ]; # to be generated after intalling nixos
+        # hostKeys = [ "/etc/secrets/initrd/ssh_host_rsa_key" ]; # to be generated after intalling nixos
+        hostKeys = [ "/home/akunito/.ssh/ssh_host_rsa_key" ]; # to be generated after intalling nixos
       };
     };
   };
@@ -55,10 +81,10 @@
   #     preLVM = false;
   #     keyFile = "/root/luks-keyfile-DATA_4TB";
   #   };
-  fileSystems."/mnt/DATA_4TB" =
-    { device = "/dev/disk/by-uuid/0c739f88-5add-4d7c-8c61-b80171341daf";
-      fsType = "ext4";
-    };
+  # fileSystems."/mnt/DATA_4TB" =
+  #   { device = "/dev/disk/by-uuid/0c739f88-5add-4d7c-8c61-b80171341daf";
+  #     fsType = "ext4";
+  #   };
 
   # systemd.services.unlock-data4tb = {
   #   description = "Unlock LUKS device for DATA_4TB";
