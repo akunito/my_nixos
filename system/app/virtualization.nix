e0967d71ecd0e2ac2ed9033a46f3ef93040e4b79
akunito <>
2024-08-14 12:13:21 +0200

    removing cockpit as virtual-machines not supported yet

--- system/app/virtualization.nix
+++ system/app/virtualization.nix
@@ -3,15 +3,11 @@
 {
   # Virt-manager doc > https://nixos.wiki/wiki/Virt-manager
   # Note there is another virtualization.nix on user folder
-<<<<<<< HEAD
-  environment.systemPackages = with pkgs; [ virt-manager distrobox ];
-=======
   environment.systemPackages = with pkgs; [
     virt-manager
     distrobox
   ];
 
->>>>>>> ec7ad38 (removing cockpit as virtual-machines not supported yet)
   programs.virt-manager.enable = true;
   virtualisation.libvirtd = {
     # To enable networks check the doc above
