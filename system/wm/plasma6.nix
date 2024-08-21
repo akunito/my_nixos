5ad136f507c670c1d3ab882e1c332328d34c4da2
akunito <diego88aku@gmail.com>
2024-08-21 15:33:20 +0200

    adding kwallet

--- system/wm/plasma6.nix
+++ system/wm/plasma6.nix
@@ -2,31 +2,27 @@
 
 {
   # Import wayland config
-  imports = [ ./wayland.nix
+  imports = [ # ./wayland.nix
               ./pipewire.nix
               ./fonts.nix
               ./dbus.nix
+              # ./gnome-keyring.nix
             ];
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  # services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
+  # Security
+  security.pam.services.kwallet = {
+    name = "kwallet";
+    enableKwallet = true;
+  };
 
   # # KDE Plasma 6
-  # services.xserver.enable = true;
-  # services.xserver.displayManager.sddm.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
   services.displayManager.sddm.wayland.enable = true; # enable if blackscreen with plasma6
-  # services.desktopManager.plasma6.enable = true;
   services.displayManager.defaultSession = "plasma";
-}
 
+  # # Enable the X11 windowing system.
+  # # You can disable this if you're only using the Wayland session.
   # services.xserver.enable = true;
   # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.displayManager.sddm.wayland.enable = true; # enable if blackscreen with plasma6
-  # services.xserver.displayManager.defaultSession = "plasma"; # or should be "plasmawayland" ??
-  # services.desktopManager.plasma6.enable = true;
+}
--- system/wm/wayland.nix
+++ system/wm/wayland.nix
@@ -3,7 +3,7 @@
 {
   imports = [ ./pipewire.nix
               ./dbus.nix
-              # ./gnome-keyring.nix
+              ./gnome-keyring.nix
               ./fonts.nix
             ];
 
