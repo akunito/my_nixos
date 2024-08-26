6ec672c22c050cf484f12fd1de2c5358cc52363b
akunito <diego88aku@gmail.com>
2024-08-26 09:22:56 +0200

    add new plasma & kde dotfiles to plasma6

--- user/wm/plasma6/plasma6.nix
+++ user/wm/plasma6/plasma6.nix
@@ -15,12 +15,45 @@
     source = ./source/autostart;
     recursive = true;
   };
-  home.file.".local/share/plasma/desktoptheme/" = { # Custom Plasma themes
-    source = ./source/desktoptheme;
+  # home.file.".local/share/plasma/desktoptheme/" = { # Custom Plasma themes
+  #   source = ./source/desktoptheme;
+  #   recursive = true;
+  # };
+  # home.file.".config/plasma-workspace/env/" = { # Env scripts run at the start of a Plasma session
+  #   source = ./source/env;
+  #   recursive = true;
+  # };
+
+  home.file.".config/kde.org/" = { # directory. Stores settings for applications related to the KDE project under the domain kde.org. This includes a variety of modern KDE applications.
+    source = ./source/kde.org;
     recursive = true;
   };
-  home.file.".config/plasma-workspace/env/" = { # Env scripts run at the start of a Plasma session
-    source = ./source/env;
+  home.file.".config/kwin/" = { # directory. Stores configurations for KWin, the window manager for Plasma. This includes window rules, shortcuts, and effects
+    source = ./source/kwin;
+    recursive = true;
+  };
+  home.file.".config/plasma-workspace/" = { # directory. Contains various configuration files related to the Plasma workspace, including desktop layout, panels, and widgets
+    source = ./source/plasma-workspace;
+    recursive = true;
+  };
+  home.file.".local/share/kactivitymanagerd/" = { # directory. Custom keybindings
+    source = ./source/kactivitymanagerd;
+    recursive = true;
+  };
+  home.file.".local/share/kded6/" = { # directory.
+    source = ./source/kded6;
+    recursive = true;
+  };
+  home.file.".local/share/plasma/" = { # directory.
+    source = ./source/plasma;
+    recursive = true;
+  };  
+  home.file.".local/share/plasmashell/" = { # directory.
+    source = ./source/plasmashell;
+    recursive = true;
+  };
+  home.file.".local/share/systemsettings/" = { # directory.
+    source = ./source/systemsettings;
     recursive = true;
   };
 
@@ -38,8 +71,28 @@
   home.file.".config/konsolerc".source = ./source/konsolerc; # Konsole terminal settings
   home.file.".config/kglobalshortcutsrc".source = ./source/kglobalshortcutsrc; # Global shortcuts
 
-  home.file.".local/share/plasma/look-and-feel".source = ./source/look-and-feel; # Look an dfeel packages
+  # home.file.".local/share/plasma/look-and-feel".source = ./source/look-and-feel; # Look an dfeel packages
   home.file.".local/share/aurorae/themes".source = ./source/themes; # Window decoration themes
   home.file.".local/share/color-schemes".source = ./source/color-schemes; # Color schemes
+
+  home.file.".config/kactivitymanagerd-pluginsrc".source = ./source/kactivitymanagerd-pluginsrc; # Configuration for plugins used by the KDE activity manager
+  home.file.".config/kactivitymanagerd-statsrc".source = ./source/kactivitymanagerd-statsrc; # Stores statistical data and settings related to KDE activities
+  home.file.".config/kactivitymanagerd-switcher".source = ./source/kactivitymanagerd-switcher; # Configuration for the activity switcher, which lets you switch between different activities
+  home.file.".config/kactivitymanagerdrc".source = ./source/kactivitymanagerdrc; # General configuration for the KDE activity manager
+  home.file.".config/kcmfonts".source = ./source/kcmfonts; # Stores font settings from the KDE control module
+  home.file.".config/kded5rc".source = ./source/kded5rc; # Configuration for the KDE Daemon (kded5), which handles various background tasks in KDE
+  home.file.".config/kded6rc".source = ./source/kded6rc; # Configuration file for kded6, the upcoming version of KDE Daemon, used in Plasma 6 or newer.
+  home.file.".config/kfontinstuirc".source = ./source/kfontinstuirc; # Stores settings for the KDE font installer interface.
+  home.file.".config/kwinrulesrc".source = ./source/kwinrulesrc; # Stores custom window rules in KWin.
+  home.file.".config/plasma-localerc".source = ./source/plasma-localerc; # Stores locale settings for the Plasma desktop
+  home.file.".config/plasmanotifyrc".source = ./source/plasmanotifyrc; # Configuration for Plasma notifications
+  home.file.".config/plasmarc".source = ./source/plasmarc; # Stores general settings for the Plasma desktop
+  home.file.".config/plasmashellrc".source = ./source/plasmashellrc; # Configuration file for the Plasma shell, which manages the desktop, panels, and widgets. Wallpapers.
+  home.file.".config/plasmawindowed-appletsrc".source = ./source/plasmawindowed-appletsrc; # Configuration for Plasma applets in windows
+  home.file.".config/plasmawindowedrc".source = ./source/plasmawindowedrc; # Configuration for Plasma windows
+  home.file.".config/powerdevilrc".source = ./source/powerdevilrc; # Configuration for Powerdevil
+  home.file.".config/powermanagementprofilesrc".source = ./source/powermanagementprofilesrc; # Configuration for Power Management Profiles
+  home.file.".config/spectaclerc".source = ./source/spectaclerc; # Configuration for Spectacle
+  home.file.".config/systemsettingsrc".source = ./source/systemsettingsrc; # Configuration for System Settings
 
 }
--- user/wm/plasma6/source/check_directories.sh
+++ user/wm/plasma6/source/check_directories.sh
@@ -7,6 +7,14 @@ DIRECTORIES=(
     "$HOME/.config/autostart"
     "$HOME/.local/share/plasma/desktoptheme"
     "$HOME/.config/plasma-workspace/env"
+    "$HOME/.config/kde.org" # directory. Stores settings for applications related to the KDE project under the domain kde.org. This includes a variety of modern KDE applications.
+    "$HOME/.config/kwin" # directory. Stores configurations for KWin, the window manager for Plasma. This includes window rules, shortcuts, and effects
+    "$HOME/.config/plasma-workspace" # directory. Contains various configuration files related to the Plasma workspace, including desktop layout, panels, and widgets
+    "$HOME/.local/share/kactivitymanagerd" # directory. Custom keybindings
+    "$HOME/.local/share/kded6" # directory. 
+    "$HOME/.local/share/plasma" # directory. 
+    "$HOME/.local/share/plasmashell" # directory. 
+    "$HOME/.local/share/systemsettings" # directory. 
 )
 
 # Loop through each directory in the array
@@ -19,3 +27,7 @@ for DIRECTORY in "${DIRECTORIES[@]}"; do
         echo "Directory $DIRECTORY created."
     fi
 done
+
+echo ""
+echo "it's possible that you have to run it few times if the order was not correct. Or you can fix the script to do it rightly"
+
