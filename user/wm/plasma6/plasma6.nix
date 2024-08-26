5d30ef0f219bfafe1130d7f33ad2bf8fde54db9b
akunito <diego88aku@gmail.com>
2024-08-26 07:12:09 +0200

    moving plasma config files

--- user/wm/plasma6/plasma6.nix
+++ user/wm/plasma6/plasma6.nix
@@ -12,33 +12,34 @@
 
   # Plasma config > Directory symlinks
   home.file.".config/autostart/" = {
-    source = ./autostart;
+    source = ./source/autostart;
     recursive = true;
   };
   home.file.".local/share/plasma/desktoptheme/" = { # Custom Plasma themes
-    source = ./desktoptheme;
+    source = ./source/desktoptheme;
     recursive = true;
   };
   home.file.".config/plasma-workspace/env/" = { # Env scripts run at the start of a Plasma session
-    source = ./env;
+    source = ./source/env;
     recursive = true;
   };
 
   # Plasma config > Files symlinks
-  home.file.".config/plasma-org.kde.plasma.desktop-appletsrc".source = ./plasma-org.kde.plasma.desktop-appletsrc; # Desktop widgets and panels config
-  home.file.".config/kdeglobals".source = ./kdeglobals; # General KDE settings
-  home.file.".config/kwinrc".source = ./kwinrc; # KWin window manager settings
-  home.file.".config/krunnerrc".source = ./krunnerrc; # KRunner settings // not found
-  home.file.".config/khotkeysrc".source = ./khotkeysrc; # Custom keybindings
-  home.file.".config/kscreenlockerrc".source = ./kscreenlockerrc; # Screen locker settings
-  home.file.".config/kwalletrc".source = ./kwalletrc; # Kwallet settings
-  home.file.".config/kcminputrc".source = ./kcminputrc; # Input settings
-  home.file.".config/ksmserverrc".source = ./ksmserverrc; # Session management settings
-  home.file.".config/dolphinrc".source = ./dolphinrc; # Dolphin file manager settings
-  home.file.".config/konsolerc".source = ./konsolerc; # Konsole terminal settings
-  home.file.".config/kglobalshortcutsrc".source = ./kglobalshortcutsrc; # Global shortcuts
-  home.file.".local/share/plasma/look-and-feel".source = ./look-and-feel; # Look an dfeel packages
-  home.file.".local/share/aurorae/themes".source = ./themes; # Window decoration themes
-  home.file.".local/share/color-schemes".source = ./color-schemes; # Color schemes
+  home.file.".config/plasma-org.kde.plasma.desktop-appletsrc".source = ./source/plasma-org.kde.plasma.desktop-appletsrc; # Desktop widgets and panels config
+  home.file.".config/kdeglobals".source = ./source/kdeglobals; # General KDE settings
+  home.file.".config/kwinrc".source = ./source/kwinrc; # KWin window manager settings
+  home.file.".config/krunnerrc".source = ./source/krunnerrc; # KRunner settings // not found
+  home.file.".config/khotkeysrc".source = ./source/khotkeysrc; # Custom keybindings
+  home.file.".config/kscreenlockerrc".source = ./source/kscreenlockerrc; # Screen locker settings
+  home.file.".config/kwalletrc".source = ./source/kwalletrc; # Kwallet settings
+  home.file.".config/kcminputrc".source = ./source/kcminputrc; # Input settings
+  home.file.".config/ksmserverrc".source = ./source/ksmserverrc; # Session management settings
+  home.file.".config/dolphinrc".source = ./source/dolphinrc; # Dolphin file manager settings
+  home.file.".config/konsolerc".source = ./source/konsolerc; # Konsole terminal settings
+  home.file.".config/kglobalshortcutsrc".source = ./source/kglobalshortcutsrc; # Global shortcuts
+
+  home.file.".local/share/plasma/look-and-feel".source = ./source/look-and-feel; # Look an dfeel packages
+  home.file.".local/share/aurorae/themes".source = ./source/themes; # Window decoration themes
+  home.file.".local/share/color-schemes".source = ./source/color-schemes; # Color schemes
 
 }
