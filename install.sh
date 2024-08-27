93a05d8d11070177ba8cd5abb68404e7a811bc46
akunito <diego88aku@gmail.com>
2024-08-27 11:08:25 +0200

    simplify firewall to allow only SSH and Syncthing | firewall.nix & sshd.nix

--- install.sh
+++ install.sh
@@ -51,23 +51,23 @@ fi
 echo "Hardening files..."
 sudo $SCRIPT_DIR/harden.sh $SCRIPT_DIR;
 
-# CLEAR all previous IPTABLES RULES
-sudo iptables -P INPUT ACCEPT
-sudo iptables -P FORWARD ACCEPT
-sudo iptables -P OUTPUT ACCEPT
-sudo iptables -t nat -F
-sudo iptables -t mangle -F
-sudo iptables -F
-sudo iptables -X
+# # CLEAR all previous IPTABLES RULES | In case you use custom IPTABLES rules
+# sudo iptables -P INPUT ACCEPT
+# sudo iptables -P FORWARD ACCEPT
+# sudo iptables -P OUTPUT ACCEPT
+# sudo iptables -t nat -F
+# sudo iptables -t mangle -F
+# sudo iptables -F
+# sudo iptables -X
 
-# CLEAR all previous IP6TABLES RULES
-sudo ip6tables -P INPUT ACCEPT
-sudo ip6tables -P FORWARD ACCEPT
-sudo ip6tables -P OUTPUT ACCEPT
-sudo ip6tables -t nat -F
-sudo ip6tables -t mangle -F
-sudo ip6tables -F
-sudo ip6tables -X
+# # CLEAR all previous IP6TABLES RULES | In case you use custom IPTABLES rules
+# sudo ip6tables -P INPUT ACCEPT
+# sudo ip6tables -P FORWARD ACCEPT
+# sudo ip6tables -P OUTPUT ACCEPT
+# sudo ip6tables -t nat -F
+# sudo ip6tables -t mangle -F
+# sudo ip6tables -F
+# sudo ip6tables -X
 
 # Rebuild system
 echo "Rebuilding system with flake..."
