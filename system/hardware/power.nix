c3d08014a888884746237f3d102716aadc092096
akunito <diego88aku@gmail.com>
2024-08-29 16:38:02 +0200

    fix power for all profiles

--- system/hardware/power.nix
+++ system/hardware/power.nix
@@ -3,17 +3,17 @@
 {
   # Overriding to disable power-profiles-daemon 
   # as it cannot work together with "tlp"
-  # services.power-profiles-daemon.enable = false;
+  services.power-profiles-daemon.enable = systemSettings.power-profiles-daemon_ENABLE;
 
   # Enable tlp service
-  services.tlp = {
+  services.tlp = lib.mkIf (systemSettings.TLP_ENABLE == true) {
     enable = true;
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "balanced";
+      CPU_SCALING_GOVERNOR_ON_AC = systemSettings.PROFILE_ON_AC;
+      CPU_SCALING_GOVERNOR_ON_BAT = systemSettings.PROFILE_ON_BAT;
 
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "balanced";
+      CPU_ENERGY_PERF_POLICY_ON_AC = systemSettings.PROFILE_ON_AC;
+      CPU_ENERGY_PERF_POLICY_ON_BAT = systemSettings.PROFILE_ON_BAT;
 
       CPU_DRIVER_OPMODE_ON_AC = "active";
       CPU_DRIVER_OPMODE_ON_BAT = "active";
@@ -34,8 +34,8 @@
       MEM_SLEEP_ON_AC = "deep";
       MEM_SLEEP_ON_BAT = "deep";
 
-      PLATFORM_PROFILE_ON_AC = systemSettings.PLATFORM_PROFILE_ON_AC;
-      PLATFORM_PROFILE_ON_BAT = systemSettings.PLATFORM_PROFILE_ON_BAT;
+      PLATFORM_PROFILE_ON_AC = systemSettings.PROFILE_ON_AC;
+      PLATFORM_PROFILE_ON_BAT = systemSettings.PROFILE_ON_BAT;
 
       WIFI_PWR_ON_AC = systemSettings.WIFI_PWR_ON_AC;
       WIFI_PWR_ON_BAT = systemSettings.WIFI_PWR_ON_BAT;
@@ -53,11 +53,15 @@
     };
   };
   
-  # powerManagement.enable = false;
-  # services.logind.lidSwitch = systemSettings.lidSwitch;
-  # services.logind.lidSwitchExternalPower = systemSettings.lidSwitchExternalPower;
-  # services.logind.lidSwitchDocked = systemSettings.lidSwitchDocked;
-  # services.logind.powerKey = systemSettings.powerKey;
+  powerManagement.enable = systemSettings.powerManagement_ENABLE;
+
+  # LOGIND
+  services.logind = lib.mkIf (systemSettings.LOGIND_ENABLE == true) {
+    lidSwitch = systemSettings.lidSwitch;
+    lidSwitchExternalPower = systemSettings.lidSwitchExternalPower;
+    lidSwitchDocked = systemSettings.lidSwitchDocked;
+    powerKey = systemSettings.powerKey;
+  };
 
   # Disable wifi powersave for Intel Network Adapter (to avoid disconnect wifi when closing the lid)
   boot.extraModprobeConfig = lib.mkIf (systemSettings.iwlwifiDisablePowerSave == true) ''
