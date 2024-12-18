6fb31da54f90b68bf442f316afc9b0455f451096
akunito <diego88aku@gmail.com>
2024-12-18 11:16:56 +0100

    Feat: adding grafana.nix as monitoring frontend tool. Sourced by homelab/base.nix Feat: adding nginx service to access grafana by https only

--- system/app/grafana.nix
+++ system/app/grafana.nix
@@ -1,19 +1,57 @@
 { pkgs, lib, systemSettings, ... }:
 
 {
+  # environment.etc."nginx/certs/akunito.org.es.cert".source = /home/akunito/.nginx/nginx-certs/akunito.org.es.crt;
+  # environment.etc."nginx/certs/akunito.org.es.key".source = /home/akunito/.nginx/nginx-certs/akunito.org.es.key;
+  # environment.etc."nginx/certs/akunito.org.es.cert".mode = "0644";
+  # environment.etc."nginx/certs/akunito.org.es.key".mode = "0600";
+
   services.grafana = {
     enable = true;
-    
     settings = {
       server = {
-        root_url = "https://monitor.akunito.org.es"; # Match your nginx-proxy hostname
+        http_addr = "127.0.0.1"; # Listening Address
+        http_port = 3002; # Listening Port
+        protocol = "http";
         domain = "monitor.akunito.org.es";
-        port = 3002; # default is 3000
         enforce_domain = true;
       };
     };
-    # # Use a custom configuration file if required
-    # extraConfigFile = "/etc/grafana/custom.ini";
   };
-  # networking.firewall.allowedTCPPorts = [ 3000 ];
+
+  services.nginx = {
+    enable = true;
+    defaultHTTPListenPort = 8040;
+    defaultSSLListenPort = 8043;
+    virtualHosts = { 
+      "monitor.akunito.org.es" = {
+        addSSL = true;
+        enableACME = false;
+        sslCertificate = "/etc/nginx/certs/akunito.org.es.crt";
+        sslCertificateKey = "/etc/nginx/certs/akunito.org.es.key";
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:3002";
+          # proxyWebsockets = true;
+          recommendedProxySettings = true;
+        };
+      };
+    };
+  };
+
+      # "default" = {
+      #   forceSSL = true; # Ensure SSL is enabled for other services
+      #   sslCertificate = "/etc/nginx/certs/akunito.org.es.crt";       # Host SSL certificate
+      #   sslCertificateKey = "/etc/nginx/certs/akunito.org.es.key";    # Host SSL key
+      #   locations = {
+      #     "/" = {
+      #       proxyPass = "https://localhost:8043"; # Proxy to Docker Nginx-proxy over HTTPS
+      #       extraConfig = ''
+      #         proxy_set_header Host $host;
+      #         proxy_set_header X-Real-IP $remote_addr;
+      #         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      #         proxy_set_header X-Forwarded-Proto https;
+      #         proxy_ssl_verify off; # Disable SSL verification for self-signed certificates
+      #       '';
+      #     };
+      #   };
 }
