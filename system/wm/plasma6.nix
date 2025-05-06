65ff1d3a3952830edd5b0f635684a361330cfbf9
akunito <diego88aku@gmail.com>
2025-05-06 14:13:03 +0200

    replace gnome-keyring by kdewallet

--- system/wm/plasma6.nix
+++ system/wm/plasma6.nix
@@ -6,22 +6,22 @@
               ./pipewire.nix
               ./fonts.nix
               ./dbus.nix
-              ./gnome-keyring.nix
+              #./gnome-keyring.nix
             ]
     ++ lib.optional userSettings.wmEnableHyprland (./. + "/hyprland.nix");
 
-  # Security
-  security = {
-    pam.services.login.enableGnomeKeyring = true;
-  };
-  services.gnome.gnome-keyring.enable = true;
+  # # Security
+  # security = {
+  #   pam.services.login.enableGnomeKeyring = true;
+  # };
+  # services.gnome.gnome-keyring.enable = true;
 
   # # KDE Plasma 6
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
   services.displayManager.sddm.wayland.enable = true; # enable if blackscreen with plasma6
   services.displayManager.defaultSession = "plasma";
-
+ 
   # # Enable the X11 windowing system.
   # # You can disable this if you're only using the Wayland session.
   # services.xserver.enable = true;
