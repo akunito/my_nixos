50b99a8fe375e381af004de150e264182f815c80
akunito <diego88aku@gmail.com>
2025-07-28 11:50:22 +0200

    Remove steam-run from firejail to install Ashes

--- system/security/firejail.nix
+++ system/security/firejail.nix
@@ -12,9 +12,9 @@
       executable = "${pkgs.steam}/bin/steam";
       profile = "${pkgs.firejail}/etc/firejail/steam.profile";
     };
-    steam-run = {
-      executable = "${pkgs.steam}/bin/steam-run";
-      profile = "${pkgs.firejail}/etc/firejail/steam.profile";
-    };
+    # steam-run = {
+    #   executable = "${pkgs.steam}/bin/steam-run";
+    #   profile = "${pkgs.firejail}/etc/firejail/steam.profile";
+    # };
   };
 }
